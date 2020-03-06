1c57a19a6d3d47eb94dac8a75ca75416a5e6e202
Ryan Dennis <rrdennis@gmail.com>
2020-03-05 22:23:31 -0800

    Updates

---
+++ precache-manifest.8be5bc2303500b572f000b439faea35e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1fcadadf43dc5e297f7a87adf245b6",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "2019b90e5afc2ed053e1",
+    "url": "/project-4-assessment/static/css/main.2e10f898.chunk.css"
+  },
+  {
+    "revision": "30e6c1223448795c9b45",
+    "url": "/project-4-assessment/static/js/2.fe9083d4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/project-4-assessment/static/js/2.fe9083d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2019b90e5afc2ed053e1",
+    "url": "/project-4-assessment/static/js/main.131e3f0b.chunk.js"
+  },
+  {
+    "revision": "710040f4d9954f54e398",
+    "url": "/project-4-assessment/static/js/runtime-main.c1c16f88.js"
+  }
+]);
